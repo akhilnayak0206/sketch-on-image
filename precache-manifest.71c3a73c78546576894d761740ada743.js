0497e1f65fa4f045a7b859a9d4064970b4d35f68
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-04-28 12:38:31 +0530

    Updates

---
+++ precache-manifest.71c3a73c78546576894d761740ada743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292fcf19ff4152bdd8ac045ddacb602b",
+    "url": "/sketch-on-image/index.html"
+  },
+  {
+    "revision": "4e1b6b421920cb2e5dd9",
+    "url": "/sketch-on-image/static/css/main.434ba0a9.chunk.css"
+  },
+  {
+    "revision": "32134a624bf2ca4921b9",
+    "url": "/sketch-on-image/static/js/2.41cf315f.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/sketch-on-image/static/js/2.41cf315f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1b6b421920cb2e5dd9",
+    "url": "/sketch-on-image/static/js/main.fd6b9176.chunk.js"
+  },
+  {
+    "revision": "9e3de6d564bb95be0005",
+    "url": "/sketch-on-image/static/js/runtime-main.f8ffd38c.js"
+  }
+]);
